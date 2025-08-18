3204fe8762f8f8fb231ec95509efa044f33ed39e
이상용 <lsy3709@naver.com>
2025-08-18 11:30:57 +0900

    3교시, NavLink 이용해서, 링크 이동 , 조건부 렌더링 확인 , 리팩토링 확인

--- ch13-vite-test/src/pages/Articles.jsx
+++ ch13-vite-test/src/pages/Articles.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // 중첩 라우팅 하기 위해서, Outlet 컴포넌트를 이용해야함.
 // import { Link, Outlet } from 'react-router-dom';
-import { NavLink, Outlet } from 'react-router-dom';
-// import ArticleItem from './ArticleItem.jsx';
+import { Outlet } from 'react-router-dom';
+import ArticleItem from './ArticleItem.jsx';
 
 //리팩토링 하기 전 원본 파일 : Articles_Backup.jsx , 참고하기.
 const Articles = () => {
@@ -19,21 +19,4 @@ const Articles = () => {
   );
 };
 
-const ArticleItem = ({ id }) => {
-  // 샘플 css
-  const activeStyle = {
-    color: 'green',
-    fontSize: '21px',
-  };
-  return (
-    <li>
-      <NavLink
-        to={`/articles/${id}`}
-        style={({ isActive }) => (isActive ? activeStyle : undefined)}
-      >
-        게시글 {id}
-      </NavLink>
-    </li>
-  );
-};
 export default Articles;
