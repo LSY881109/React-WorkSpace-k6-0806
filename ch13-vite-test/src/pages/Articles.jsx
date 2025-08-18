baef417e751ce0f730c81f41ddbdf5616b062f98
이상용 <lsy3709@naver.com>
2025-08-18 11:26:59 +0900

    3교시, NavLink 이용해서, 링크 이동 , 조건부 렌더링 확인 , 리팩토링 중

--- ch13-vite-test/src/pages/Articles.jsx
+++ ch13-vite-test/src/pages/Articles.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 // 중첩 라우팅 하기 위해서, Outlet 컴포넌트를 이용해야함.
-import { Link, Outlet } from 'react-router-dom';
+// import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+// import ArticleItem from './ArticleItem.jsx';
 
+//리팩토링 하기 전 원본 파일 : Articles_Backup.jsx , 참고하기.
 const Articles = () => {
   return (
     <div>
       {/*중첩 라우팅 적용하기.*/}
       <Outlet />
       <ul>
-        <li>
-          <Link to="/articles/1">게시글 1</Link>
-        </li>
-        <li>
-          <Link to="/articles/2">게시글 2</Link>
-        </li>
-        <li>
-          <Link to="/articles/3">게시글 3</Link>
-        </li>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
       </ul>
     </div>
   );
 };
 
+const ArticleItem = ({ id }) => {
+  // 샘플 css
+  const activeStyle = {
+    color: 'green',
+    fontSize: '21px',
+  };
+  return (
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
+  );
+};
 export default Articles;
