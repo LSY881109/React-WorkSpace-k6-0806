0b6d384f9cb786aad93c8a5b0e9286b7f91eb197
이상용 <lsy3709@naver.com>
2025-08-19 11:12:39 +0900

    3교시, 카테고리 작업4, 라우팅 설정 및 확인2

--- ch14-vite-test/src/components/NewsList.jsx
+++ ch14-vite-test/src/components/NewsList.jsx
@@ -17,14 +17,6 @@ const NewsListBlock = styled.div`
     padding-right: 1rem;
   }
 `;
-// 더미 샘플 데이터 이용.
-// const sampleArticle = {
-//   title: '제목',
-//   description: '내용',
-//   url: 'http://example.com/',
-//   urlToImage:
-//     'https://sportshub.cbsistatic.com/i/r/2025/08/16/253debd9-29a2-48cd-8b31-501631451ee8/thumbnail/1200x675/db792d9aa7d3d3d9326347b93bd6439c/dricus-khamzat-weighins.jpg',
-// };
 
 const NewsList = ({ category }) => {
   //실제 데이터 연동,
@@ -41,7 +33,7 @@ const NewsList = ({ category }) => {
         const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
           // 'https://newsapi.org/v2/top-headlines?country=us&apiKey=본인꺼',
-          `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=b7adb4f936494b3bac62f446ab7686cb`,
+          `https://newsapi.org/v2/top-headlines?country=us${query}&apiKey=본인꺼`,
         );
         setArticles(response.data.articles);
         console.log(response.data);
@@ -58,12 +50,6 @@ const NewsList = ({ category }) => {
       {articles.map((article) => (
         <NewsItem key={article.url} article={article} />
       ))}
-
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
-      {/*<NewsItem article={sampleArticle} />*/}
     </NewsListBlock>
   );
 };
